7511fadfe4ff43f65538b39c68ce6c1acfbb9b64
wangwei <673209859@qq.com>
2019-02-25 22:27:58 +0800

    Initial commit

---
+++ microservice-consumer-movie/src/main/java/com/itmuch/cloud/study/user/controller/MovieController.java
@@ -0,0 +1,24 @@
+package com.itmuch.cloud.study.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.itmuch.cloud.study.user.entity.User;
+
+@RestController
+public class MovieController {
+  @Autowired
+  private RestTemplate restTemplate;
+
+//  private static String REST_URL_PREFIX = "http://localhost:8000";
+  private static String REST_URL_PREFIX = "http://MICROSERVICE-PROVIDER-USER";
+
+
+  @GetMapping("/user/{id}")
+  public User findById(@PathVariable Long id) {
+    return this.restTemplate.getForObject(REST_URL_PREFIX+"/" + id, User.class);
+  }
+}
---
+++ microservice-provider-user1/src/main/java/com/itmuch/cloud/study/ProviderUser1Application.java
@@ -0,0 +1,13 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProviderUser1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderUser1Application.class, args);
+    }
+}
