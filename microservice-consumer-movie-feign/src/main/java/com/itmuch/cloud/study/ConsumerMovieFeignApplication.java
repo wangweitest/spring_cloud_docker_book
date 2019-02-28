a5e03e78bbd8657c51020210752acf16946b650b
wangwei <362@op110.com>
2019-02-28 17:31:43 +0800

    修改feign负载均衡策略，漏掉的文件

---
+++ microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/study/ConsumerMovieFeignApplication.java
@@ -0,0 +1,27 @@
+package com.itmuch.cloud.study;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableFeignClients
+public class ConsumerMovieFeignApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerMovieFeignApplication.class, args);
+  }
+
+  @Bean
+  public IRule iRule(){
+    return new RandomRule();
+  }
+
+}
+
+
+
